e7a019d14c7af3fa3582914fa8d769a43a406a4c
plutoese <glen.zhang7@gmail.com>
2023-05-08 08:45:06 +0800

    wagtail v0.21

---
+++ projects/wagtail_app/wagtail_demo_02/sevendays/sevendays/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-#rht62yf*(511yzx&fr1#ko$uk0+anfy4#2$t1&ulh7cj+((%9"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
